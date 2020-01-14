9a9fd16c8c23352a60e8eff186bdff9554291c80
Subhi Pandey <subhi2012cpi@gmail.com>
2020-01-15 00:10:28 +0530

    Minor Changes

--- app/src/main/java/com/example/mvpexample/MainActivity.kt
+++ app/src/main/java/com/example/mvpexample/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.mvpexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+//import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+
+    private fun rollDice() {
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
